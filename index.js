b45c9d066eb30e7ad5c09a7021af613bd2c5386e
clinton <clinton.wambua@student.moringaschool.com>
2023-04-09 09:35:31 +0300

    completed

--- index.js
+++ index.js
@@ -1,30 +1,77 @@
-const API_KEY = "YOUR API KEY"
+const API_KEY = "b72d1d8b6c7fe9a44f73fcdeda90fe0d";
 
 function handleFormSubmit(event) {
-  //handle submit event
+  event.preventDefault();
+  const cityInput = document.getElementById("city").value;
+  fetchCurrentWeather(cityInput);
+  fetchFiveDayForecast(cityInput);
 }
 
 function fetchCurrentWeather(city) {
-  //fetch current weather based on city
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => displayCurrentWeather(json));
 }
 
 function displayCurrentWeather(json) {
-  //render current weather data to the DOM using provided IDs and json from API
+  document.getElementById("temp").textContent = `${json.main.temp}°F`;
+  document.getElementById("low").textContent = `${json.main.temp_min}°F`;
+  document.getElementById("high").textContent = `${json.main.temp_max}°F`;
+  document.getElementById("humidity").textContent = `${json.main.humidity}%`;
+  document.getElementById("cloudCover").textContent = `${json.clouds.all}%`;
+  document.getElementById("sunrise").textContent = `${new Date(json.sys.sunrise * 1000).toLocaleTimeString()}`;
+  document.getElementById("sunset").textContent = `${new Date(json.sys.sunset * 1000).toLocaleTimeString()}`;
 }
 
-
 function fetchFiveDayForecast(city) {
-  //fetch five day forecast data based on city
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => displayFiveDayForecast(json));
 }
 
 function displayFiveDayForecast(json) {
-  //render five day forecast data to the DOM using provided IDs and json from API
+  const forecastSection = document.querySelector('aside');
+  forecastSection.innerHTML = '';
+  for (let i = 0; i < json.list.length; i+=8) {
+    const forecastItem = document.createElement('div');
+    forecastItem.classList.add('forecast-item');
+    forecastItem.innerHTML = `
+      <h3>${new Date(json.list[i].dt_txt).toLocaleDateString()}</h3>
+      <p>${json.list[i].weather[0].description}</p>
+      <p>${json.list[i].main.temp}°F</p>
+      <img src="http://openweathermap.org/img/w/${json.list[i].weather[0].icon}.png"/>
+    `;
+    forecastSection.appendChild(forecastItem);
+  }
 }
 
 function createChart(json) {
-  //Bonus: render temperature chart using five day forecast data and ChartJS
+  const tempData = json.list.map(item => item.main.temp);
+  const dateLabels = json.list.map(item => new Date(item.dt_txt).toLocaleDateString());
+  const ctx = document.getElementById('WeatherChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dateLabels,
+      datasets: [{
+        label: 'Temperature (°F)',
+        data: tempData,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: false
+        }
+      }
+    }
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  //add event listener here for form submission
-})
+  const cityForm = document.getElementById('cityForm');
+  cityForm.addEventListener('submit', handleFormSubmit);
+});
